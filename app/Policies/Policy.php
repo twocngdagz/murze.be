2cf46ca977a19c9623afb18a67f25e0333b65543
Mederic Roy Beldia <twocngdagz@yahoo.com>
2018-02-24 15:36:52 +0800

    add Content Policy Security

---
+++ app/Policies/Policy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Policy as BasePolicy;
+
+class Policy extends BasePolicy
+{
+    public function configure()
+    {
+        return $this
+            ->addDirective(Directive::BASE, 'self')
+            ->addDirective(Directive::CONNECT, 'self')
+            ->addDirective(Directive::DEFAULT, 'self')
+            ->addDirective(Directive::FORM_ACTION, 'self')
+            ->addDirective(Directive::IMG, 'self')
+            ->addDirective(Directive::MEDIA, 'self')
+            ->addDirective(Directive::SCRIPT, 'self')
+            ->addDirective(Directive::STYLE, 'self')
+            ->addDirective(Directive::FONT, 'self')
+            ->addDirective(Directive::OBJECT, 'none')
+            ->addDirective(Directive::SCRIPT, '*.google-analytics.com')
+            ->addDirective(Directive::SCRIPT, 'fonts.googleapis.com')
+            ->addDirective(Directive::STYLE, 'fonts.googleapis.com')
+            ->addDirective(Directive::FONT, 'fonts.gstatic.com')
+            ->addDirective(Directive::FONT, 'data:')
+            ->addNonceForDirective(Directive::SCRIPT)
+            ->addNonceForDirective(Directive::STYLE);
+    }
+}
